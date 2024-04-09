4834b27b3f57d8f4188774f3bbd2565358d7778a
tcaballero95 <tomas.caballero1995@gmail.com>
2024-04-09 15:40:14 -0400

    streamlit webapp requirements
    Librerias utilizadas para el día 9/4/2024

--- pages/4_🌧️_Ingresa tus Datos.py
+++ pages/4_🌧️_Ingresa tus Datos.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title='Pluviómetros Ciudadanos DGF', layout="wide")
 
 st.header('Ingresa tus Datos')
 
-with st.form(key='my_form'):
+with st.form(key='my_form', border=False):
 	text_input = st.text_input(label='Ingresa tu email')
 	event = st.selectbox('Seleccione el evento del dato a registrar', ('2024/Junio/23', '2024/Agosto/08', '2024/Julio/14'))
 	number_input = st.number_input(label='Ingresa la precipitación registrada en [mm]')
