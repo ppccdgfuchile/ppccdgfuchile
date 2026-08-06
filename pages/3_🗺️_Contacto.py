6a40b7bc02e60993435982717a3cc78da06f0832
ppccdgfuchile <ppcc.dgf.uchile@gmail.com>
2026-08-06 15:44:30 -0400

    Update 3_🗺️_Contacto.py
    sacar ohiggins uvalpo y cr2

--- pages/3_🗺️_Contacto.py
+++ pages/3_🗺️_Contacto.py
@@ -14,18 +14,15 @@ ciudadana que se desarrolló en el
 Chile](https://ingenieria.uchile.cl/) con el objetivo de estudiar los 
 efectos topográficos sobre la distribución espacial de la precipitación 
 en la Región Metropolitana, a partir de mediciones realizadas por 
-observadores(as) voluntarios(as) y de estaciones meteorológicas automáticas 
-(EMA) de la red nacional. El proyecto comenzó en el 2021 ampliándose en el 
-2024 a las regiones de Valparaíso (V) y O'higgins (VI), a través de 
-iniciativas similares coordinadas con el 
-[Departamento de Meteorología de la Universidad de Valparaiso](https://meteo.uv.cl) 
-y el 
-[Instituto de Ciencias de la Ingeniería de la Universidad de O'Higgins](https://www.uoh.cl/instituto-de-ciencias-de-la-ingenieria/).
+observadores(as) voluntarios(as) e información de estaciones meteorológicas automáticas 
+(EMA) pertenecientes o coordinadas por varios organismos, entre ellos la Dirección
+Meteorológica de Chile (DMC) y la Dirección General de Aguas (DGA).
+El proyecto comenzó en el 2021.
 
-La información de precipitación que se obtiene a través de este proyecto 
-es de libre acceso y se publica en este sitio Web en forma de tablas 
-(viñeta Registros) y mapas (viñeta Mapas). Los puntos de medición de los(as) 
-observadores que colaboran con el proyecto se identifican mediante un alias.
+La información de precipitación que se obtiene a través de este proyecto es
+de libre acceso y se publica en este sitio Web en forma de tablas (viñeta Registros)
+y mapas (viñeta Mapas). Los puntos de medición de los(as) observadores que colaboran con
+el proyecto se identifican mediante un alias.
 
 Al hacer un click sobre un punto de medición en el mapa se despliega 
 información sobre el nombre de la estación meteorológica o el alias del 
@@ -34,27 +31,13 @@ una caracterización (Grupo) con los códigos EMA si corresponde a una
 estación meteorológica automática o RM, V-R, VI-R para identificar a qué 
 filial del proyecto pertenece la persona que realiza la observación.
 
-Los contactos para quienes deseen colaborar con este proyecto son los 
-siguientes, dependiendo de la región de residencia:
+El contacto para quienes deseen colaborar con este proyecto es el siguiente:
 
-* Región Metropolitana:
-    Prof. Patricio Aceituno, Dpt. de Geofísica, FCFM - U. de Chile.
-    Correo electrónico: aceituno@uchile.cl
-
-* Región de Valparaíso:
-    Prof. Ana María Córdoba, Dpt. de Meteorología, U. de Valparaíso.
-    Correo electrónico: anamaria.cordova@uv.cl
-
-* Región de O'Higgins:
-    Prof. Raúl Valenzuela, Instituto de Ciencias de la Ingeniería, U. de 
-    O'Higgins.
-    Correo electrónico: raul.valenzuela@uoh.cl
+Prof. Patricio Aceituno, Dpt. de Geofísica, FCFM - U. de Chile.
+Correo: aceituno@uchile.cl
 """)
 
-path1, path2, path3, path4, path5 = [os.path.join('static', f) for f in
-                                     ['logo_ppcc.png', 'logo_dgf.png',
-                                      'logo_uvalpo.png', 'logo_uoh.png',
-                                      'logo_cr2.png']]
+path1, path2 = [os.path.join('static', f) for f in ['logo_ppcc.png', 'logo_dgf.png']]
 st.sidebar.image(path1, use_container_width=True)
 
 
@@ -63,9 +46,9 @@ with cols[0]:
     st.image(path1, use_container_width=True)
 with cols[2]:
     st.image(path2, use_container_width=True)
-with cols[4]:
-    st.image(path3, use_container_width=True)
-with cols[6]:
-    st.image(path4, use_container_width=True)
-with cols[8]:
-    st.image(path5, use_container_width=True)
+# with cols[4]:
+#     st.image(path3, use_container_width=True)
+# with cols[6]:
+#     st.image(path4, use_container_width=True)
+# with cols[8]:
+#     st.image(path5, use_container_width=True)
